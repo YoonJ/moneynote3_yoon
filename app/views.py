19fa6b8c082944844610d409d40d6410059f4f6c
YoonJ <oyoons95@gmail.com>
2018-05-24 19:58:21 +0900

    firebase connect04

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
+from dbconnect import insert_data
+
+#temp_data = get_data('김채우오')
+#print(temp_data)
 
 # Create your views here.
 def keyboard(request):
@@ -8,21 +13,24 @@ def keyboard(request):
     return JsonResponse(
         {
         'type': 'buttons',
-        'buttons': ['1번','2번']
+        'buttons': ['데이터 생성']
         }
     )
 
 @csrf_exempt
 def message(request):
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content']
 
-    return JsonResponse(
-        {
+    if return_str == '데이터 생성':
+        insert_data()
+        return JsonResponse({
             'message':{
-                'text':'메세지를 써라'
+                'text':'데이터 삽입'
             },
             'keyboard':{
                 'type': 'buttons',
-                'buttons': ['1번', '2번','3번']
+                'buttons': ['데이터 생성]
             }
-        }
-    )
+        })
